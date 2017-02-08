3a9b3c93e35caaf67b6f9e8a6333b9e97a6eccd6
Rohohertz <hitmaxtom@gmail.com>
2017-02-08 17:48:09 +0800

    ok

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /program/ugv_ws/src/kitti_player
-# Build directory: /program/ugv_ws/src/kitti_player/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
--- build/setup_custom_pythonpath.sh
+++
@@ -1,4 +0,0 @@
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/program/ugv_ws/src/kitti_player/build/devel/lib/python2.7/dist-packages:$PYTHONPATH
-exec "$@"
